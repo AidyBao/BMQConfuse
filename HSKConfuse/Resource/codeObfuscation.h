751e61afa44fb03e0c99fd76603b592de4ca3148
AidyBao <120v@AidyBao.local>
2017-03-29 16:18:14 +0800

    1.0.1

---
+++ HSKConfuse/Resource/codeObfuscation.h
@@ -0,0 +1,8 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Wed Mar 29 16:12:46 CST 2017
+#define hsk_Function1 WzaJIpauyeugOeMs
+#define hsk_Function2 BSJtNVLaJfSOlPZE
+#define hsk_Function3 qkHguehljuZwRnSx
+#define hsk_start UlKwDMEqSOUhbSjW
+#endif
